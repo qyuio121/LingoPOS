44c4f8f7e428e82400bcffbb9919a1f656fe75fb
민정우 <qbladep@naver.com>
2018-10-17 22:19:53 +0900

    예약페이지에 footer에 생기는 화살표 제거

--- src/main/webapp/js/search.js
+++ src/main/webapp/js/search.js
@@ -37,7 +37,7 @@ function setCurrentPosition(){
 	    		mapInfo.markers.push(marker);
 	    		var overlay = new daum.maps.CustomOverlay({
 	    			   content : ('<div class="container">' + 
-		   	    	              '    <div class="row">' + 
+		   	    	              '    <div class="row id="markerRow">' + 
 		   	    	              '        <div class="title">' +
 		   	    	              '            <div class ="titleText" style="width:250px">'+
 		   	    	              '            <a href="'+'#'+'" class="link">'+value.storename +'</a></div>'+
