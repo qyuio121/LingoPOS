c799a503a033f2e9c57c09c1892ed4fb2542900c
최창선 <heom@naver.com>
2018-10-18 18:28:12 +0900

    로그인 컨트롤러 안드로이드 연결

--- src/main/java/com/kosmo/lingopos/LoginController.java
+++ src/main/java/com/kosmo/lingopos/LoginController.java
@@ -5,11 +5,13 @@ import java.util.Map;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -42,5 +44,33 @@ public class LoginController {
 	public String logout(SessionStatus status) throws Exception{
 		status.setComplete();
 		return "index.tiles";
+	}
+	
+	//최창선 안드로이드 로그인 프로세스
+	@ResponseBody
+	@RequestMapping(value="/AndroidLogin/LoginProcess.Lingo",produces="text/plain; charset=UTF-8")
+	public String androidLoginprocess(@RequestParam Map map, Model model) throws Exception{
+		LoginDTO loginDTO = loginService.select(map);
+		JSONObject json = new JSONObject();
+		if(loginDTO!=null) {
+			if(loginDTO.getOwnerno() != null) {
+				json.put("isMember","seller");
+				System.out.println("점주에 들어옴");
+				return json.toJSONString();
+			}
+			else if(loginDTO.getAdminno() != null) {
+				json.put("isMember","admin");
+				System.out.println("관리자에 들어옴");
+				return json.toJSONString();
+			}
+			else
+				json.put("isMember","customer");
+			System.out.println("고객에 들어옴");
+				return json.toJSONString();
+		}else {
+			json.put("isMember","false");
+			System.out.println("그냥 틀린거에 들어옴");
+			return json.toJSONString();
+		}
 	}
 }
