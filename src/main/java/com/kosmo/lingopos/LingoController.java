660733b627cd5d9503d56aed1b4463c6bda3ec43
userpc_x1x1 <userpc_x1x1@DESKTOP-397IKK9>
2018-10-19 02:47:46 +0900

    로컬 서버 코드 / 원격 코드 주석처리

--- src/main/java/com/kosmo/lingopos/LingoController.java
+++ src/main/java/com/kosmo/lingopos/LingoController.java
@@ -133,8 +133,8 @@ public class LingoController {
 		mhsr.setAttribute("realImage", newFilename);
 		mhsr.setAttribute("totalImage", upload.getOriginalFilename());
 		//6]서머노트에 데이타 전달
-		//return "https://"+localIP+ ":"+mhsr.getLocalPort() +"/lingopos/Images/summernote/" + newFilename;
-		return "https://www.lingopos.co.kr/lingopos/Images/summernote/" + newFilename;
+		return "https://"+localIP+ ":"+mhsr.getLocalPort() +"/lingopos/Images/summernote/" + newFilename;
+		//return "https://www.lingopos.co.kr/lingopos/Images/summernote/" + newFilename;
 	}
 	//창선 사진 삭제 - 서머노트 Controller
 	@ResponseBody
@@ -180,8 +180,8 @@ public class LingoController {
 				String realAddress = phicalPath+File.separator+newFilename;
 				//로컬 호스트 전달값
 				String localIP = InetAddress.getLocalHost().getHostAddress();
-				//String localAddress = "https://"+localIP+ ":"+mhsr.getServerPort() +"/lingopos/Images/"+dto.getId()+"/store/"+ newFilename;
-				String localAddress = "https://www.lingopos.co.kr/lingopos/Images/"+dto.getId()+"/store/"+ newFilename;
+				String localAddress = "https://"+localIP+ ":"+mhsr.getServerPort() +"/lingopos/Images/"+dto.getId()+"/store/"+ newFilename;
+				//String localAddress = "https://www.lingopos.co.kr/lingopos/Images/"+dto.getId()+"/store/"+ newFilename;
 				
 				record.put("realAddress",realAddress);
 				record.put("localAddress",localAddress);
@@ -231,8 +231,8 @@ public class LingoController {
 				String realAddress = phicalPath+File.separator+newFilename;
 				//로컬 호스트 전달값
 				String localIP = InetAddress.getLocalHost().getHostAddress();
-				//String localAddress = "https://"+localIP+ ":"+mhsr.getServerPort() +"/lingopos/Images/"+dto.getId()+"/menu/"+ newFilename;
-				String localAddress = "https://www.lingopos.co.kr/lingopos/Images/"+dto.getId()+"/menu/"+ newFilename;
+				String localAddress = "https://"+localIP+ ":"+mhsr.getServerPort() +"/lingopos/Images/"+dto.getId()+"/menu/"+ newFilename;
+				//String localAddress = "https://www.lingopos.co.kr/lingopos/Images/"+dto.getId()+"/menu/"+ newFilename;
 				
 				record.put("realAddress",realAddress);
 				record.put("localAddress",localAddress);
