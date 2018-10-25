9ea9b0d4ec1076a2e96358aaa43709693846ca8b
최창선 <heom@naver.com>
2018-10-25 10:40:49 +0900

    안드로이드 제휴가게 기능 구현

--- src/main/java/com/kosmo/lingopos/TempleteController.java
+++ src/main/java/com/kosmo/lingopos/TempleteController.java
@@ -1,11 +1,26 @@
 package com.kosmo.lingopos;
 
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+import java.util.HashMap;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.kosmo.lingopos.map.MapDTO;
+import com.kosmo.lingopos.map.MapService;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.json.simple.JSONArray;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 @Controller
 @SessionAttributes("loginDTO")
 public class TempleteController {
+	
+	@Resource(name="mapService")
+	private MapService mapService;
 	
 	//백엔드 블랙리스트신청
 	@RequestMapping("/Admin/blackList/blackApply.Admin")
@@ -19,5 +34,21 @@ public class TempleteController {
 	public String blackList() throws Exception{
 		return "shop/blackList/blackList.tiles";
 	}	
-
+	
+	//안드로이드 토탈 맵 
+	@ResponseBody
+	@RequestMapping(value="/Android/TotalMap.Lingo",produces="text/html; charset=UTF-8")
+	public String getmap() throws Exception{
+		List<MapDTO> list = mapService.select();
+		List<Map> maplist= new Vector<Map>();
+		for(MapDTO dto:list) {
+			Map map = new HashMap();
+			map.put("x",dto.getX());
+			map.put("y",dto.getY());
+			map.put("storename",dto.getStorename());
+			maplist.add(map);
+		}
+		System.out.println(maplist);
+		return JSONArray.toJSONString(maplist);
+	}
 }
